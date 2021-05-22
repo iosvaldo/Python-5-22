4ebeb4f4f5478bcd077e2649f3665bb443eb2578
iosvaldo <35927834+iosvaldo@users.noreply.github.com>
2021-05-22 16:42:24 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def SplitTheBill(ppl,bill,tip):
+  share = (1+ tip)* bill/ppl
+  share = round(share,2)
+  return share
+  
+# print(SplitTheBill(5,100.13))
+
+# print(f" I went to dinner last night with 4 friends and I spent ${SplitTheBill(5,61.61,0.15)}.")
+
+animal = input("What is your Favorite animal?")
+
+print(animal)
